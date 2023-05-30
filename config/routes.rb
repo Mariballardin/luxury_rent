33c14721a41671da85504c765851970f06dbbf7c
Mateo Tomljanovic <bswagmateo@gmail.com>
2023-05-30 16:13:15 +0200

    Cretaed 3 routes for renting

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :products, only: [:index, :show, :new, :create, :destroy]
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :products, only: [:index, :show, :new, :create, :destroy] do
+    resources :reservations, only: [:new, :create, :destroy, :edit, :update]
+  end
 end
