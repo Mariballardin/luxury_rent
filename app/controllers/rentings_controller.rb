ca4260b9fc78568afb226b5ee0e00d6abddd4bf7
Armin Plath <armin.plath@bavaresca.de>
2023-05-30 17:35:46 +0200

    logo added and size changed to 80px

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -1,2 +1,10 @@
 class RentingsController < ApplicationController
+  def index
+  end
+
+  def destroy
+    @renting = Renting.find(params[:id])
+    @renting.destroy
+    redirect_to rentings_path, notice: 'Renting was successfully deleted.'
+  end
 end
