a12d20bc0904997fa46dab3bedecac794f73d5c8
Mariana Ballardin <mariana.ballardin@hotmail.com>
2023-06-01 17:49:34 +0200

    amazing reservation delete and confirm now working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   resources :products, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
-    resources :rentings, only: [:new, :create, :destroy, :edit, :update]
+    resources :rentings, only: [:new, :create, :destroy, :edit]
   end
   resources :rentings, only: [:index]
 end
